7bfd4c5989981248eed45eaa337faf95df200577
youth_k <youth_k0228@outlook.com>
2019-05-16 23:31:29 +0900

    5/16 ソースコード完成

--- No1.php
+++ No1.php
@@ -17,3 +17,40 @@ if(isset($_POST["send"]) === true){
         $message="書き込みに成功しました";
     }
 }
+
+$fp=fopen("data.txt","r");
+
+$dataArr=array();
+while($res=fgets($fp)){
+    $tmp=explode("\t", $res);
+    $arr=array(
+        "name" => $tmp[0],
+        "comment" => $tmp[1]
+    );
+    $dataArr[]=$arr;
+}
+?>
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="ja">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+		<title>掲示板</title>
+	</head>
+	<body>
+		<?php echo $message; ?>
+		<form method="post" action="">
+		名前：<input type="text"name="name" value="<?php echo $name;?>" >
+			  <?php echo $err_msg1; ?><br>
+			  コメント：<textarea name="comment" rows="4" cols="40"><?php echo $comment; ?></textarea>
+			  <?php echo $err_msg2; ?><br>
+<br>
+			 <input type="submit" name="send" value="クリック">
+		</form>
+		<dl>
+		<?php foreach($dataArr as $data):?>
+		<p><span><?php echo $data["name"]; ?></span>:<span><?php echo $data["comment"]; ?></span></p>
+		<?php endforeach;?>
+		</dl>
+	</body>
+</html>
